287d72d3d41ebc1c2e211c74850ca92b48c87f21
Jason Kim <active_red@hotmail.com>
2014-04-10 00:19:24 -0700

    travis code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,16 @@ Dir["#{pathname_root_module}/*.rb"].each {|file| require file}
 
 RSpec.configure do |config|
 end
+
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+  module Kernel
+    alias :__at_exit :at_exit
+    def at_exit(&block)
+      __at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        block.call
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
